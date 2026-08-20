6fa6f92de2706bbc5c88f62291079a43426b1234
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-20 13:06:48 +0300

    Update CanPayHook.swift

--- Sources/asasecmod/Hooks/CanPayHook.swift
+++ Sources/asasecmod/Hooks/CanPayHook.swift
@@ -6,5 +6,12 @@ struct CanPayHook: Hook {
 
     let cls: AnyClass? = SKPaymentQueue.self
     let sel: Selector = #selector(SKPaymentQueue.canMakePayments)
-    let replace: T = { _, _ in true }
+    let replace: T = { obj, sel in
+        // Eğer bedava satın alma kapalıysa orijinal fonksiyonu çalıştır
+        if !Preferences.isFreePurchaseEnabled {
+            return orig(obj, sel)
+        }
+        // Açıksa her zaman true döndür
+        return true
+    }
 }
